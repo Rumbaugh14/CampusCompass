330165c143853fda11a4b045dfe9004d86ab6600
akroner <ark5531@psu.edu>
2017-12-05 22:40:58 -0500

    akroner added database stuff for profile

--- app/src/main/java/com/example/shane/campuscompass/DatabaseHelperProfile.java
+++ app/src/main/java/com/example/shane/campuscompass/DatabaseHelperProfile.java
@@ -1,8 +1,65 @@
 package com.example.shane.campuscompass;
 
-/**
- * Created by andre on 12/5/2017.
- */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelperProfile {
+/**
+ * Created by Andrew
+ */
+public class DatabaseHelperProfile extends SQLiteOpenHelper {
+    private static final String TAG = "DatabaseHelperProfile";
+    private static final String TABLE_NAME = "profile_table";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "name";
+    private static final String COL3 = "major";
+    private static final String COL4 = "email";
+
+    public DatabaseHelperProfile(Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "Create TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL2 +" TEXT, " + COL3 +" TEXT, " + COL4 +" TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String upgrade = "DROP IF TABLE EXISTS "  + TABLE_NAME;
+        db.execSQL(upgrade);
+        onCreate(db);
+    }
+
+    public boolean addData(String name, String major, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, name);
+        contentValues.put(COL3, major);
+        contentValues.put(COL4, email);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void delete() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String delete = "DELETE * FROM " + TABLE_NAME;
+        db.execSQL(delete);
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT " + COL2 + ", " + COL3 + ", " + COL4 + " FROM " + TABLE_NAME, null);
+        return data;
+    }
 }
--- app/src/main/java/com/example/shane/campuscompass/ProfileFragment.java
+++ app/src/main/java/com/example/shane/campuscompass/ProfileFragment.java
@@ -1,10 +1,12 @@
 package com.example.shane.campuscompass;
 
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,12 +17,18 @@ import android.widget.TextView;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.PopupWindow;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Andrew
  */
 public class ProfileFragment extends Fragment {
 
+    private static final String TAG  = "ProfileFragment";
     private Button editButton; //profile ui edit button
     private TextView profileName;
     private TextView profileMajor;
@@ -33,6 +41,7 @@ public class ProfileFragment extends Fragment {
     private String name = "Name";
     private String major = "Major";
     private String email = "E-Mail";
+    DatabaseHelperProfile mDatabaseHelper;
 
 
     public ProfileFragment() {
@@ -44,11 +53,13 @@ public class ProfileFragment extends Fragment {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.profile_fragment, container, false);
 
-        editButton = (Button) v.findViewById(R.id.editButton);
+        editButton = (Button)v.findViewById(R.id.editButton);
         profileName = (TextView)v.findViewById(R.id.userName);
         profileMajor = (TextView)v.findViewById(R.id.userMajor);
         profileEmail = (TextView)v.findViewById(R.id.userEmail);
+        mDatabaseHelper = new DatabaseHelperProfile(getContext());
 
+        populateProfile();
         return v;
     }
 
@@ -67,15 +78,14 @@ public class ProfileFragment extends Fragment {
         View popupView = getLayoutInflater().inflate(R.layout.profile_pop_window, null);
 
         profilePopup = new PopupWindow(popupView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        profilePopup.setFocusable(true);
+        profilePopup.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        profilePopup.showAtLocation(anchorView, Gravity.NO_GRAVITY, 0, 0);
 
         saveButton = (Button) popupView.findViewById(R.id.saveChanges);
         editName = (EditText) popupView.findViewById(R.id.nameField);
         editMajor = (EditText) popupView.findViewById(R.id.majorField);
         editEmail = (EditText) popupView.findViewById(R.id.emailField);
-
-        profilePopup.setFocusable(true);
-        profilePopup.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-        profilePopup.showAtLocation(anchorView, Gravity.NO_GRAVITY, 0, 0);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -83,10 +93,43 @@ public class ProfileFragment extends Fragment {
                 major = editMajor.getText().toString();
                 email = editEmail.getText().toString();
 
+                if (editName.length() != 0 && editMajor.length() != 0 && editEmail.length() != 0) {
+                    addData(name, major, email, view);
+                } else {
+                    toastMessage("One or more fields do not contain data!", view);
+                }
+
                 profileName.setText(name);
                 profileMajor.setText(major);
                 profileEmail.setText(email);
+                profilePopup.dismiss();
             }
         });
+    }
+
+    private void toastMessage(String message, View v) {
+        Toast.makeText(v.getContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void addData(String newEntry1, String newEntry2, String newEntry3, View v) {
+        boolean insertData = mDatabaseHelper.addData(newEntry1, newEntry2, newEntry3);
+
+        if(insertData) {
+            toastMessage("Profile information updated!", v);
+        } else {
+            toastMessage("Something went wrong.", v);
+        }
+    }
+
+    private void populateProfile () {
+        Log.d(TAG, "ProfileFragment: Attempting to display data to the profile.");
+
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+        while(data.moveToNext()) {
+            profileName.setText(data.getString(0));
+            profileMajor.setText(data.getString(1));
+            profileEmail.setText(data.getString(2));
+        }
     }
 }
